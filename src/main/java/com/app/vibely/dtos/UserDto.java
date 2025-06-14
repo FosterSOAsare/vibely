d61509c10a43d65e481a945d7d7d6adcf46a87f2
FosterSOAsare <fostersoasare@gmail.com>
2025-06-14 00:54:59 +0000

    Lombok setup for dtos and mappers

--- src/main/java/com/app/vibely/dtos/UserDto.java
+++ src/main/java/com/app/vibely/dtos/UserDto.java
@@ -3,6 +3,7 @@ package com.app.vibely.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 
@@ -10,6 +11,7 @@ import java.time.Instant;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 public class UserDto {
     private String username;
     private Integer id;
